ec740e19b592afc32b198525dd324d23c39ec147
id01 <gaviniboom@gmail.com>
2017-03-14 20:37:33 -0700

    modified:   bootkali.c 	modified:   installkalichroot.sh

--- bootkali.c
+++ bootkali.c
@@ -2,13 +2,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void cleanupxhost() { system("/usr/bin/xhost -local:"); }
+
 // Do not compile this outside of the installkalichroot.sh script. It won't work.
 // Nifty little binary that doesn't need changes to sudo :D
 int main()
 {
+	// Verify that user either root or kali
 	int myuid = getuid();
 	if (myuid == kaliuid || myuid == 0) { setuid(0); } else { puts("Permission denied."); return 1; }
-	puts("Entering kali chroot...");
-	char *const argvs[] = {chrootpath, "/home/kali/kaliroot", NULL};
-	execv(chrootpath, argvs);
+	puts("Entering Kali chroot...");
+	// Allow xhost connection from anyone
+	if (usesxhost == 1) { system("/usr/bin/xhost +local:"); atexit(cleanupxhost); }
+	// Run chroot, wait for exit
+	system(chrootcommand);
+	puts("Exitted Kali chroot.");
+	// Exit
+	exit(EXIT_SUCCESS);
 }
--- installkalichroot.sh
+++ installkalichroot.sh
@@ -26,9 +26,11 @@ echo "Added user kali. Starting setup process..."
 mkdir /home/kali
 mkdir /home/kali/kaliroot
 set +e
+
 # Install debootstrap if they haven't already, create the chroot
 apt-get -y install debootstrap
 debootstrap --arch $ARCH $VERSION /home/kali/kaliroot http://deb.debian.org/debian
+
 # Add kali repos and install kali base system
 echo "# Kali repositories" >> "/home/kali/kaliroot/$APTPATH"
 echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> "/home/kali/kaliroot/$APTPATH"
@@ -37,9 +39,15 @@ chroot /home/kali/kaliroot sh -c 'apt-get update && apt-get --force-yes -y insta
 set +e
 chroot /home/kali/kaliroot sh -c 'apt-get -y install sudo kali-linux' # Install kali base system
 chroot /home/kali/kaliroot sh -c 'apt-get -y install kali-linux' # Install twice, just in case...
+
+# Update Locales
+chroot /home/kali/kaliroot sh -c 'locale-gen "en_US.UTF-8"; echo "LANG=en_US.UTF-8" >> /etc/default/locale'
+
 # Create bootkali and modify user to chroot
 rm /tmp/bootkali_tmp.c
-echo "#define chrootpath \"`which chroot`\"" > /tmp/bootkali_tmp.c
+echo "#define chrootcommand \"sh -c '`which chroot` /home/kali/kaliroot'\"" > /tmp/bootkali_tmp.c
+xhost | grep "access control disabled" 2>&1 >/dev/null
+echo "#define usesxhost $?" >> /tmp/bootkali_tmp.c
 echo "int kaliuid = `sudo -u kali id -u`;" >> /tmp/bootkali_tmp.c
 cat bootkali.c >> /tmp/bootkali_tmp.c
 gcc /tmp/bootkali_tmp.c -o /home/kali/bootkali
@@ -47,11 +55,13 @@ chmod 4755 /home/kali/bootkali
 rm /tmp/bootkali_tmp.c
 usermod -s /bin/bash kali
 echo "exec /home/kali/bootkali" > /home/kali/.bashrc
+
 # Update /etc/fstab
 echo -e "\n" >> /etc/fstab
 echo "# Kali" >> /etc/fstab
 echo -e "/proc\t/home/kali/kaliroot/proc none defaults,bind 0 0" >> /etc/fstab
 echo -e "/sys\t/home/kali/kaliroot/sys none defaults,bind 0 0" >> /etc/fstab
+
 # Final message
 echo "Done. Use sudo su kali to chroot into kali"
 echo "Note: Setting a password for the kali user is not a good idea."
